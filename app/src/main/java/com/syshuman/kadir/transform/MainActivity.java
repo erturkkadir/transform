59ca73b75351d8122f705c5ef98726a4506ec97f
Kadir <kadirerturk@gmail.com>
2017-10-14 17:27:03 -0700

    GL setup

--- app/src/main/java/com/syshuman/kadir/transform/MainActivity.java
+++ app/src/main/java/com/syshuman/kadir/transform/MainActivity.java
@@ -3,10 +3,15 @@ package com.syshuman.kadir.transform;
 import android.app.Activity;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
+import android.opengl.GLU;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -16,14 +21,74 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+
+    private GLSurfaceView.Renderer renderer;
+
+    @BindView(R.id.surfaceView) GLSurfaceView surfaceView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        surfaceView.setEGLContextClientVersion(2);
+
+        renderer = new GLSurfaceView.Renderer() {
+            @Override
+            public void onSurfaceCreated(GL10 gl10, EGLConfig eglConfig) {
+                gl10.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);  // Set color's clear-value to black
+                gl10.glClearDepthf(1.0f);            // Set depth's clear-value to farthest
+                gl10.glEnable(GL10.GL_DEPTH_TEST);   // Enables depth-buffer for hidden surface removal
+                gl10.glDepthFunc(GL10.GL_LEQUAL);    // The type of depth testing to do
+                gl10.glHint(GL10.GL_PERSPECTIVE_CORRECTION_HINT, GL10.GL_NICEST);  // nice perspective view
+                gl10.glShadeModel(GL10.GL_SMOOTH);   // Enable smooth shading of color
+                gl10.glDisable(GL10.GL_DITHER);      // Disable dithering for better performance
+                surfaceView.setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
+
+            }
+
+            @Override
+            public void onSurfaceChanged(GL10 gl10, int width, int height) {
+               // GLES20.glViewport(0,0,width, length);
+
+                if (height == 0) height = 1;   // To prevent divide by zero
+                float aspect = (float)width / height;
+
+                // Set the viewport (display area) to cover the entire window
+                gl10.glViewport(0, 0, width, height);
+
+                // Setup perspective projection, with aspect ratio matches viewport
+                gl10.glMatrixMode(GL10.GL_PROJECTION); // Select projection matrix
+                gl10.glLoadIdentity();                 // Reset projection matrix
+                // Use perspective projection
+                GLU.gluPerspective(gl10, 45, aspect, 0.1f, 100.f);
+
+                gl10.glMatrixMode(GL10.GL_MODELVIEW);  // Select model-view matrix
+                gl10.glLoadIdentity();                 // Reset
+            }
+
+            @Override
+            public void onDrawFrame(GL10 gl10) {
+                gl10.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
+            }
+        };
+
+        surfaceView.setRenderer(renderer);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -43,10 +108,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        FragmentManager fragmentManager = getFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.add(R.id.frameFrouier, new FourierFragment());
-        fragmentTransaction.commit();
+
     }
 
     @Override
@@ -65,8 +127,15 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     }
 
     @Override
+    public void onResume() {
+        super.onResume();
+        surfaceView.onResume();
+    }
+
+    @Override
     public void onPause() {
         super.onPause();
+        surfaceView.onPause();
     }
 
     @Override
